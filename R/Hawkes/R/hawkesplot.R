8977d845ec93f8f4e7bfbb1a487cc7151c5714c9
AndrewC1998 <andrew.connell19@imperial.ac.uk>
2020-05-08 19:24:10 +0100

    08/05/20 Fixes the plotting tool for the processes. AC

--- R/Hawkes/R/hawkesplot.R
+++ R/Hawkes/R/hawkesplot.R
@@ -1,17 +1,17 @@
 Hawkes.plot <- function(data, type = "Count"){
   if(type == "Count"){
-    plot(1:length(data$N[,1]), data$N[,1], type = "l",
-         col = "blue", xlab = "t", ylab = "N")
+    plot(data$r, data$N[,1], type = "l",
+         col = "blue", xlab = "Time", ylab = "Counting Process")
     for(i in 2:length(data$N[1,])){
-      lines(1:length(data$N[,i]), data$N[,i], col = i + 10*i)
+      lines(data$r, data$N[,i], col = i + 10*i)
     }
     grid(20,20)
   }else if(type == "Intensity"){
-    plot(1:length(data$intensity[,1]), data$intensity[,1], type = "l",
-         col = "blue", xlab = "t", ylab = "lambda_{m}(t)",
+    plot(test$r, data$intensity[,1], type = "l",
+         col = "blue", xlab = "Time", ylab = "lambda_{m}(t)",
          ylim = c(floor(min(test$intensity)), ceiling(max(test$intensity))))
     for(i in 2:length(data$intensity[1,])){
-      lines(1:length(data$intensity[,i]), data$intensity[,i], col = i + 10*i)
+      lines(data$r, data$intensity[,i], col = i + 10*i)
     }
     grid(20,20)
   }else{
