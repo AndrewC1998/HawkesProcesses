144e90b8450411e510568ddb3c87cc5e9ca92402
AndrewC1998 <andrew.connell19@imperial.ac.uk>
2020-05-15 20:44:18 +0100

    15/05/20 AC

---
+++ R/Hawkes/R/hawkesll.R
@@ -0,0 +1,63 @@
+Hawkes.ll <- function(t, mu, alpha, beta, P = 1){
+  # t is matrix of times each col is M=1,2,...
+  # mu is vector of base intensities
+  # alpha is a square matrix of alpha coefficients
+  # beta is a qsuare matrix of beta coefficients
+  # P is model order
+
+  # Create recursive formula
+  if(P==1){
+    R <- function(i,m,d){
+      tmp <- 0
+      if(d == 1){
+        return(0)
+      }else{
+        for(k in 2:length(t[,m])){
+          if(t[k,i] < t[d,m]){
+            tmp <- tmp + exp(-(beta[i,m])*(t[d,m]-t[k,i]))
+          }
+        }
+        return(tmp)
+      }
+    }
+    # create vector to store likelihoods
+    ll <- c()
+    M <- length(mu)
+
+    for(m in 1:M){
+      # First for loop helps us find each dimension
+      # Find the first term
+      tmpleft <- 0
+      maxt <- max(t[,m])
+      wmax <- which.max(t[,m])
+      for(d in 1:wmax){
+        # This one for data length
+        tmp1 <- 0
+        for(i in 1:M){
+          tmp1 <- tmp1 + alpha[i,m]*R(i,m,d)
+        }
+        tmpleft <- tmpleft + log(mu[m] + tmp1)
+      }
+
+      tmpright <- 0
+      tmp2 <- c()
+      for(i in 1:M){
+        tmp3 <- 0
+        for(d in 1:wmax){
+          tmpab <- alpha[i,m]*beta[i,m]
+          tmp3 <- tmpab*(1 - exp(-(beta[i,m])*(maxt - t[d,i])))
+        }
+        tmp2[i] <- tmp3
+      }
+      tmpright <- sum(tmp2)
+
+      # Sum all terms for mth likelihood
+      ll[m] <- tmpleft - mu[m]*maxt - tmpright
+    }
+    return(sum(ll))
+  }else{
+    # If P > 1, we need generate arrays
+    # mu needs to be a function
+    return("High order P not supported yet.")
+  }
+}
