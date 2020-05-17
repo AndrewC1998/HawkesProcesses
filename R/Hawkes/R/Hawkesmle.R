0b20b1fa44355db7ed7f5639dada119078b9bd1e
AndrewC1998 <andrew.connell19@imperial.ac.uk>
2020-05-17 18:48:58 +0100

    17/05/20 Basic MLE search. AC

--- R/Hawkes/R/Hawkesmle.R
+++ R/Hawkes/R/Hawkesmle.R
@@ -1,3 +1,23 @@
-Hawkes.mle <- function(params){
-  1
+Hawkes.mle <- function(alpha, beta, M, t, mu, threshold = 10){
+  # alpha min max decisions
+  # beta min max decisions
+  # M number of dimensions
+  # t is the data we have avaiable
+  # mu is means
+
+  a <- seq(alpha[1], alpha[2], length.out = threshold)
+  b <- seq(beta[1], beta[2], length.out = threshold)
+  tmp <- matrix(0, nrow = threshold, ncol = threshold)
+
+  for(i in 1:threshold){
+    for(j in 1:threshold){
+      tmp[i,j] <- Hawkes.ll(t, mu, as.matrix(a[i]), as.matrix(b[j]))
+    }
+  }
+  maxval <- max(tmp)
+  maxpoint <- which(tmp == maxval, arr.ind = TRUE)
+  print(tmp)
+  return(paste(c('alpha:'), a[maxpoint[1,1]], c(', beta:'), a[maxpoint[1,2]], c(', likelihood:'), maxval))
 }
+
+Hawkes.mle(alpha = c(0.1,10), beta = c(0.1,10), M = 1, t = as.matrix(test$r), mu = c(1))
